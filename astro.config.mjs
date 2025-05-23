33cd2d9320397e313e934ae03d399c0d49c1e881
Ben Juntilla <ben@benjuntilla.com>
2025-05-22 19:11:34 -0700

    fix import error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { fileURLToPath } from 'url';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
 			title: 'ASU CS Wiki',
       customCss: [
         '@fontsource-variable/space-grotesk',
-        './src/styles/custom.css',
+        fileURLToPath(new URL('./src/styles/custom.css', import.meta.url)),
       ],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/asusoda/asu-cs-wiki' }],
       editLink: {
